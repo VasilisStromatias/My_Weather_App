b027f6a01b70916337643f64196d5c41462b9061
vstromatias <stromatiasvasilis@gmail.com>
2023-09-24 16:02:01 +0300

    ...

--- my_weather_app/src/App.js
+++ my_weather_app/src/App.js
@@ -1,4 +1,5 @@
-import React, {useState } from 'react';
+import React, {useEffect, useState } from 'react';
+import axios from 'axios';
 
 import './App.css';
 import WeatherForm from './components/weatherForm';
@@ -6,10 +7,29 @@ import WeatherOutput from './components/weatherOutput';
 
 function App() {
 
+  const lat = '38.03491'
+  const lon = '23.73811'
+  const WEATHER_API_KEY = '8f81e9f0ce84e9bd3fe8f09a74cdd101'
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+
   const [city, setCity] = useState({name: '', temp : 0})
   const sessionData = {city, setCity}
 
-  console.log(city)
+
+  useEffect(() => {
+    axios.get(url)
+    .then( response =>  {
+      console.log(response.data)
+      const data = response.data
+      setCity( {
+        ...city, name:data.name, temp : data.main.temp
+      })
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  },city)
+  
 
   return (
     <>
--- my_weather_app/src/components/weatherForm.js
+++ my_weather_app/src/components/weatherForm.js
@@ -1,9 +1,19 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 
 
 function WeatherForm({data}) {
+const GEO_API_KEY = 'I2JJ7BWpAGsTSemsqLvGCLe6vSpzm6M6';
+const c = 'Lamia';
+const url = `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${GEO_API_KEY}?q=${c}`
 
 const [input, setInput] = useState('')
+
+// useEffect(() => {
+// axios.get(url)
+// .then (response => console.log(response))
+// .catch(error => console.log('Error Axios'))
+// })
 
 function handleSubmit(e) {
     e.preventDefault();
